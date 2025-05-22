b828d82bb04a2710804266156fc1414b5f485501
AleksandrosP38 <alexandrepacheco2000@hotmail.com>
2025-05-22 13:26:52 +0100

    level lock system

--- Assets/Scripts/GridBased/LevelLoader.cs
+++ Assets/Scripts/GridBased/LevelLoader.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -9,19 +9,31 @@ public class LevelLoader : MonoBehaviour
     public GameObject star2;
     public GameObject star3;
 
+    private Button levelButton; // The button component
+
     void Start()
     {
+        levelButton = GetComponent<Button>(); // Get the button on this object
+
+        // Show stars based on saved data
         string key = "Level_" + levelBuildIndex + "_Stars";
         int starsEarned = PlayerPrefs.GetInt(key, 0);
 
         if (star1 != null) star1.SetActive(starsEarned >= 1);
         if (star2 != null) star2.SetActive(starsEarned >= 2);
         if (star3 != null) star3.SetActive(starsEarned >= 3);
-    }
 
-    public void LoadSceneByName(string sceneName)
-    {
-        SceneManager.LoadScene(sceneName); // Load by name (e.g., "Level1")
+        // ✅ Lock logic: disable button if previous level has 0 stars
+        if (levelBuildIndex > 1) // Level 1 is always unlocked
+        {
+            string prevKey = "Level_" + (levelBuildIndex - 1) + "_Stars";
+            int prevStars = PlayerPrefs.GetInt(prevKey, 0);
+
+            if (prevStars == 0 && levelButton != null)
+            {
+                levelButton.interactable = false;
+            }
+        }
     }
 
     public void LoadSceneByIndex(int buildIndex)
